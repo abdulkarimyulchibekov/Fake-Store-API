90dcbb11596e2636be059878d90d0618f93b2f50
abdulkarimyulchibekov <abdulkarimyulchibekov@gamil.com>
2023-02-13 14:43:14 +0500

    Change tests

--- cypress/e2e/carts-page.cy.ts
+++ cypress/e2e/carts-page.cy.ts
@@ -1,8 +1,11 @@
 describe('product-page', () => {
+	before(() => {
+		cy.intercept('https://fakestoreapi.com/carts').as('getCarts');
+	});
+
 	it('first test', () => {
 		cy.login();
 		cy.getByData('carts-link').click();
-		cy.intercept('https://fakestoreapi.com/carts').as('getCarts');
 		cy.wait('@getCarts', { timeout: 10000 }).then(() => {
 			cy.location('pathname').should('eq', '/carts');
 			cy.get(':nth-child(1) > .item__link').click();
--- cypress/e2e/categories-page.cy.ts
+++ cypress/e2e/categories-page.cy.ts
@@ -1,10 +1,13 @@
 describe('category-page test', () => {
-	it('test', () => {
-		cy.login();
-		cy.getByData('categories-link').click();
+	before(() => {
 		cy.intercept('https://fakestoreapi.com/products/categories').as(
 			'getCategories',
 		);
+	});
+
+	it('test', () => {
+		cy.login();
+		cy.getByData('categories-link').click();
 		cy.wait('@getCategories', { timeout: 10000 }).then(() => {
 			cy.location('pathname').should('eq', '/categories');
 			cy.get(':nth-child(1) > .item__link')
